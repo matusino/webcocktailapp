1f1d0cca89442872979b5a400b0cdd31963311f2
Matus <matusino.sykora@gmail.com>
2020-04-10 19:49:40 +0200

    Spring Initializr

---
+++ src/main/java/com/webcocktailapp/WebCocktailApp/WebCocktailAppApplication.java
@@ -0,0 +1,13 @@
+package com.webcocktailapp.WebCocktailApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCocktailAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCocktailAppApplication.class, args);
+	}
+
+}
